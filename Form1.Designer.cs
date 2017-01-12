629029cbd95ccf911addc6ba1ee898846033616c
robHumphres <robhumphres@gmail.com>
2017-01-12 09:03:59 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Amx_Emailing_Service
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_WelcomeToAmx = new System.Windows.Forms.Label();
+            this.label_EnterEmail = new System.Windows.Forms.Label();
+            this.textBox_userEnterEmail = new System.Windows.Forms.TextBox();
+            this.checkBox_AddInfo = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_AddAdditionalInfo = new System.Windows.Forms.TextBox();
+            this.button_Submit = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_WelcomeToAmx
+            // 
+            this.label_WelcomeToAmx.AutoSize = true;
+            this.label_WelcomeToAmx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_WelcomeToAmx.Location = new System.Drawing.Point(85, 28);
+            this.label_WelcomeToAmx.Name = "label_WelcomeToAmx";
+            this.label_WelcomeToAmx.Size = new System.Drawing.Size(302, 36);
+            this.label_WelcomeToAmx.TabIndex = 0;
+            this.label_WelcomeToAmx.Text = "      Welcome to the DGX Emailing Service\r\nTo get started click on the help butto" +
+    "n above.\r\n";
+            // 
+            // label_EnterEmail
+            // 
+            this.label_EnterEmail.AutoSize = true;
+            this.label_EnterEmail.Location = new System.Drawing.Point(85, 93);
+            this.label_EnterEmail.Name = "label_EnterEmail";
+            this.label_EnterEmail.Size = new System.Drawing.Size(311, 13);
+            this.label_EnterEmail.TabIndex = 1;
+            this.label_EnterEmail.Text = "Please Enter a email below that you want the email to be sent to.";
+            // 
+            // textBox_userEnterEmail
+            // 
+            this.textBox_userEnterEmail.Location = new System.Drawing.Point(141, 109);
+            this.textBox_userEnterEmail.Name = "textBox_userEnterEmail";
+            this.textBox_userEnterEmail.Size = new System.Drawing.Size(198, 20);
+            this.textBox_userEnterEmail.TabIndex = 2;
+            this.textBox_userEnterEmail.TextChanged += new System.EventHandler(this.EnterEmailAddress);
+            // 
+            // checkBox_AddInfo
+            // 
+            this.checkBox_AddInfo.AutoSize = true;
+            this.checkBox_AddInfo.Location = new System.Drawing.Point(183, 193);
+            this.checkBox_AddInfo.Name = "checkBox_AddInfo";
+            this.checkBox_AddInfo.Size = new System.Drawing.Size(105, 17);
+            this.checkBox_AddInfo.TabIndex = 3;
+            this.checkBox_AddInfo.Text = "Add information?";
+            this.checkBox_AddInfo.UseVisualStyleBackColor = true;
+            this.checkBox_AddInfo.CheckedChanged += new System.EventHandler(this.addedInfoCheckBoxChange);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Would you like to add any additional information \r\n                    default me" +
+    "ssage sent is \r\n            \"Your DGX system is upgraded.\"";
+            // 
+            // textBox_AddAdditionalInfo
+            // 
+            this.textBox_AddAdditionalInfo.Enabled = false;
+            this.textBox_AddAdditionalInfo.Location = new System.Drawing.Point(45, 216);
+            this.textBox_AddAdditionalInfo.Multiline = true;
+            this.textBox_AddAdditionalInfo.Name = "textBox_AddAdditionalInfo";
+            this.textBox_AddAdditionalInfo.Size = new System.Drawing.Size(370, 80);
+            this.textBox_AddAdditionalInfo.TabIndex = 5;
+            // 
+            // button_Submit
+            // 
+            this.button_Submit.Enabled = false;
+            this.button_Submit.Location = new System.Drawing.Point(401, 344);
+            this.button_Submit.Name = "button_Submit";
+            this.button_Submit.Size = new System.Drawing.Size(75, 23);
+            this.button_Submit.TabIndex = 6;
+            this.button_Submit.Text = "Submit";
+            this.button_Submit.UseVisualStyleBackColor = true;
+            this.button_Submit.Click += new System.EventHandler(this.button_Submit_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(488, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.startToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Enabled = false;
+            this.button_Cancel.Location = new System.Drawing.Point(12, 344);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_Cancel.TabIndex = 8;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 379);
+            this.Controls.Add(this.button_Cancel);
+            this.Controls.Add(this.button_Submit);
+            this.Controls.Add(this.textBox_AddAdditionalInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox_AddInfo);
+            this.Controls.Add(this.textBox_userEnterEmail);
+            this.Controls.Add(this.label_EnterEmail);
+            this.Controls.Add(this.label_WelcomeToAmx);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DGX\'s Mailing Service";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_WelcomeToAmx;
+        private System.Windows.Forms.Label label_EnterEmail;
+        private System.Windows.Forms.TextBox textBox_userEnterEmail;
+        private System.Windows.Forms.CheckBox checkBox_AddInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_AddAdditionalInfo;
+        private System.Windows.Forms.Button button_Submit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button_Cancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Threading;
+using System.IO;
+
+namespace Amx_Emailing_Service
+{
+    public partial class Form1 : Form
+    {
+        private RS232.RS232 mConn;
+        private bool mBreakThread = true;
+        private bool fatalError = false;
+        private string fatalErrorLine = "";
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// Exits program completely
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            Application.Exit();
+        }//end of exitToolStrip
+
+        /// <summary>
+        /// opens Getting_Started form and explains on how this program works.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form gtForm = new Getting_Started();
+            gtForm.Show();
+        }//end of about strip
+
+        /// <summary>
+        /// Shows version and that I made it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version: v1.0.3 Author: Robert Humphres"+Environment.NewLine + "Fixed issue with doing File -> Exit not letting Comm port go and added a dgx done upgrading dialog.");
+        }
+
+        /// <summary>
+        /// Checks to see if the information is filled out
+        /// if isnt' will fire message boxes on what is needed to be fixed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Submit_Click(object sender, EventArgs e)
+        {
+            
+            if (!(textBox_userEnterEmail.Text.Equals("")) && IsValidEmail(textBox_userEnterEmail.Text))
+            {
+                Thread thread;
+                button_Submit.Enabled = false;
+                button_Cancel.Enabled = true;
+                mBreakThread = true;// True will have the thread to start looking for the complete string upgrade inside the thread and false will break the thread early.
+                StartTheConnection();
+                if (mConn.Connected)
+                {
+                    thread = new Thread(new ThreadStart(waitForKeyWord));
+                    thread.Start();
+                    //MessageBox.Show("Search has begun.");
+                }
+                    
+            }//end of inner if
+            else
+            {
+                //Could put a local bool that checks if first true and second make a combined mbox
+                if(textBox_userEnterEmail.Text.Equals(""))
+                    MessageBox.Show("You need to add email address for the program to send to.");
+                if(checkBox_AddInfo.Enabled==true)
+                    if(textBox_AddAdditionalInfo.Text.Equals(""))
+                        MessageBox.Show("Either you need to Add information to the additional info box or uncheck the \"Add Information?\" checkbox");
+                if(!(IsValidEmail(textBox_userEnterEmail.Text)))
+                    MessageBox.Show("The Email you entered is not valid.");
+
+            }//end of else
+        }//end of Submit Click Button
+
+        /// <summary>
+        /// Waiting for the DGX to send the Key word of **** System Upgrade Complete, Ready to reboot ****
+        /// </summary>
+        private void waitForKeyWord()
+        {
+            System.DateTime timeout, endtime;
+            System.TimeSpan duration;
+            bool finished = true;
+            while (finished == true)
+            {
+                //This is a of delaying the thread.....
+                duration = new System.TimeSpan(0, 0, 0, 0, 1000);
+                timeout = System.DateTime.Now;
+                endtime = timeout.Add(duration);
+
+                while (timeout < endtime)
+                {
+                    timeout = System.DateTime.Now;
+                }
+                //delay timer 
+
+                foreach (string curLine in mConn.getBuffer())
+                {
+                    //Console.WriteLine(curLine); //this shows me whats being printed out... its like a debug
+
+                    //This is executed when the Cancel button on the Winform is created, this causes the thread to finish before finding the System upgrade string.
+                    if (mBreakThread == false)
+                    {
+                        finished = false;
+                        break;
+                    }
+                        
+                    if (curLine.Contains("**** System Upgrade Complete, Ready to reboot ****"))
+                    {
+                        SendEmail();
+                        finished = false;// this takes it out of the while loop of the whole program
+                        break;
+                    }//end contains is true
+
+                    if (curLine.Contains("fatal"))
+                    {
+                        // this is a serious error should be reported to the user with the full fatal error.
+                        fatalError = true;
+                        fatalErrorLine = curLine;
+                    }
+
+                }//end of foreach
+
+            }//end of while(true) duration loop
+            
+            
+        }//end of waitingForKeyWord
+
+        /// <summary>
+        /// Cancels sending the email. Will Stop the following:
+        /// Stop the Comm1 connection
+        /// Stop the email service by stopping the thread.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            ClosePort();
+            if(mConn.Connected)
+            {
+                MessageBox.Show("Your Connection was never established in the first place.");
+            }
+
+            //This will always need to happen.
+            ResetButtons();
+
+        }
+
+        /// <summary>
+        /// OnClick Property for Entering Email so once this is clicked the submit button will be enabled.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EnterEmailAddress(object sender, EventArgs e)
+        {
+            if (!(textBox_userEnterEmail.Text.Equals("")))
+                button_Submit.Enabled = true;
+        }//end of enterEmailAddress
+
+        
+
+        /// <summary>
+        /// Connects to the Rs232 Port
+        /// After the rs232 port finds what its looking for it'll fire off an email to the user.
+        /// </summary>
+        private void StartTheConnection()
+        {
+            mConn = new RS232.RS232("1", 115200);
+            if (mConn.OpenSerialConnection() == false)
+                {
+                    MessageBox.Show("It looks like your Comm 1 port is tied up somewhere");
+                    button_Submit.Enabled = true;
+                    button_Cancel.Enabled = false;
+                }//end of failing to start comm port
+
+            else 
+                mConn.StartListening();
+
+        }//end of StartTheConnection
+
+        /// <summary>
+        /// Check and see if its a valid email. By validation it doesn't mean that it'll know if its a real one, but just the simple things like @hotmail etc.
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Tries to open an existing outlook, but if there isn't one will open a new instance of it.
+        /// Adds information from Additional Added information and who the email is going to get sent to. 
+        /// </summary>
+        private void SendEmail()
+        {
+            //Member Variables
+            Outlook.Application oApp;
+            Outlook.MailItem oMsg;
+            Outlook.Recipients oRecips;
+            Outlook.Recipient oRecip;
+
+
+            try
+            {
+                //Tries to pull open the current open one and if it's not there it'll open a new instance of it
+                try
+                {
+                    oApp = (Outlook.Application)Marshal.GetActiveObject("Outlook.Application");
+                }//end of inner try
+
+                //catch exception on instance not open
+                catch (Exception)
+                {
+                    //Tries opening a new instance of outlook if not open.
+                    oApp = new Outlook.Application();
+                }//end of catch
+
+                //Create new message
+                oMsg = (Outlook.MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
+
+                oMsg.HTMLBody = BodyMessage();
+                
+                oMsg.Subject = "Your DGX upgrade";
+                oRecips = (Outlook.Recipients)oMsg.Recipients;
+                // Add reciever for email typed by user.
+                oRecip = (Outlook.Recipient)oRecips.Add(textBox_userEnterEmail.Text);
+                oRecip.Resolve();
+                // Send.
+                oMsg.Send();
+
+                // Clean up.
+                oRecip = null;
+                oRecips = null;
+                oMsg = null;
+                oApp = null;
+
+                MessageBox.Show("Your DGX has finished upgrading.");
+                ResetButtons();
+            }//end of Try
+            catch (Exception e)
+            {
+
+                WriteToLog("there was an error sending through outlook.");
+                WriteToLog(e.ToString());
+                //MessageBox.Show(e.ToString());
+            }
+        }//end of SendEmail
+
+        /// <summary>
+        /// This method decides what the Body Message will be
+        /// This will get fired on in SendEmail
+        /// </summary>
+        /// <returns></returns>
+        private string BodyMessage()
+        {
+            if (checkBox_AddInfo.Enabled == false)
+                return "Your machine is done...";
+
+            if (fatalError)
+                return fatalErrorLine;
+
+            else
+                return readAdditionalInfoLines();
+        }//end of BodyMessage
+
+        /// <summary>
+        /// If the checkbox Addition info is checked this method will go through the multiple lines that it has and will return a string that will attach into the email.
+        /// This method will fire off inside BodyMessage()
+        /// </summary>
+        /// <returns></returns>
+        private string readAdditionalInfoLines()
+        {
+            string temp = "";
+            foreach (string tempBody in textBox_AddAdditionalInfo.Lines)
+                temp += tempBody;
+            return temp;
+        }
+
+        /// <summary>
+        /// This is a addInfoCheckboxProperty change event that will either enable the textbox or disable it for editing. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addedInfoCheckBoxChange(object sender, EventArgs e)
+        {
+            if (checkBox_AddInfo.Enabled == true)
+                textBox_AddAdditionalInfo.Enabled = true;
+            else
+                textBox_AddAdditionalInfo.Enabled = false;
+        }
+
+        /// <summary>
+        /// Resets the Cancel and Submit Button
+        /// </summary>
+        private void ResetButtons()
+        {
+            if(InvokeRequired)
+            {
+                MethodInvoker method = new MethodInvoker(ResetButtons);
+                Invoke(method);
+                return;
+            }
+
+            button_Submit.Enabled = true;
+            button_Cancel.Enabled = false;
+        }
+
+        /// <summary>
+        /// This writes a log that will be able to be sent to me so i can change the error occuring. 
+        /// </summary>
+        /// <param name="log"></param>
+        private void WriteToLog(String log)
+        {
+            MessageBox.Show("There has been a error. Please Report this error to Robert.Humphres@harman.com so I can find a fix for your error. Thank you." + Environment.NewLine + 
+                "The log is called ErrorLogMAILINGSERVICE.txt and it'll be the located in the same area as this .exe");
+
+            using (StreamWriter writetext = new StreamWriter("ErrorLogMAILINGSERVICE.txt"))
+            {
+                writetext.WriteLine(log);
+            }
+        }
+
+        private void ClosePort()
+        {
+            if (mConn.Connected == true)
+            {
+                mBreakThread = false;
+                mConn.StopListening();
+                mConn.ClearBuffer();
+                mConn.CloseSerialConnection();
+            }
+        }
+    }//end of form
+}
